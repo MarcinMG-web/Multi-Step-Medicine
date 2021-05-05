f1e8bc6cd280749fd182391902a2deb1a4a86f77
MarcinMG-web <gierada.marcin@gmail.com>
2021-05-05 10:08:15 +0200

    Add scss and mock therapy

--- src/components/pages/Therapy.js
+++ src/components/pages/Therapy.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 const Therapy = () => {
-    return (
-        <div>
-            Therapy
-        </div>
-    )
-}
 
-export default Therapy
+const [questions, setQuestions] = useState([]);
+
+  return (
+    <div>
+      Therapy
+      <div className='container'>
+        {/* Therapy*/}
+        <div>What therapy or consultation <br /> are you looking for?</div>
+        <div className='container_question'>
+
+        </div>
+        <div className='container_buttons'>
+          <Link type='submit' className='btn_back' to='./'>
+            Back
+          </Link>
+          <Link type='submit' className='btn_next' to='./therapy'>
+            Next
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Therapy;
--- src/components/pages/UserForm.js
+++ src/components/pages/UserForm.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const UserForm = () => {
-    return (
-        <div>
-            UserForm
-        </div>
-    )
-}
+  return (
+    <div className='container'>
+      {/* UserForm */}
 
-export default UserForm
+      <div className='container_buttons'>
+        <Link type='submit' className='btn_next' to='./therapy'>
+          Next
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserForm;
