0d44eddfadc22dc3deedef794a8b77306363f30c
MarcinMG-web <gierada.marcin@gmail.com>
2021-05-07 13:22:49 +0200

    Add mock list of therapist

--- src/components/pages/ListOfTherapist.js
+++ src/components/pages/ListOfTherapist.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ListOfTherapist = () => {
-    return (
-        <div>
-            ListOfTherapist
-        </div>
-    )
-}
+  return (
+    <div>
+      ListOfTherapist
+      <div className='containerTherapist'>
+        <div>ListOfTherapist</div>
 
-export default ListOfTherapist
+        <div className='containerTherapist_main'>
+          <div className='containerTherapist_buttons'>
+            <Link type='submit' className='btn_back' to='./language'>
+              Back
+            </Link>
+            <button type='submit' >
+            <Link type='submit' className='btn_next' to='./succes'>
+                 Next
+            </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListOfTherapist;
