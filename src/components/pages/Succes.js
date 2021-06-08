ea267a9769a25ee22abf532071f54b1c37eb8bc7
MarcinMG-web <gierada.marcin@gmail.com>
2021-06-08 14:43:00 +0200

    Added layout in succes.js

--- src/components/pages/Succes.js
+++ src/components/pages/Succes.js
@@ -10,20 +10,52 @@ const Succes = () => {
   const language = useSelector((state) => state.answerReducer.language);
 
   return (
-    <div>
-      <p>therapy: {therapy}</p>
-      <p>gender: {gender}</p>
-      <p>gender: {old}</p>
-      <p>issue: {issue}</p>
-      <p>language: {language}</p>
+    <div className='containerSucces'>
+      <div className='containerSucces-header'>
+        <h1>Summary:</h1>
+      </div>
+      <div className='containerSucces-results'>
+        <div className='containerSucces-results-single'>
+          <p>therapy: {therapy}</p>
+          <Link to='/' className='btn_changed'>
+            Change issues
+          </Link>
+        </div>
 
-      <Link to='/' className='btn_back'>
-        Start
-      </Link>
+        <div className='containerSucces-results-single'>
+          <p>gender: {gender}</p>
+          <Link to='/gender' className='btn_changed'>
+            Change gender
+          </Link>
+        </div>
 
-      <Link to='/language' className='btn_back'>
-        Back
-      </Link>
+        <div className='containerSucces-results-single'>
+          <p>old: {old}</p>
+          <Link to='/old' className='btn_changed'>
+            Change old
+          </Link>
+        </div>
+
+        <div className='containerSucces-results-single'>
+          <p>issue: {issue}</p>
+          <Link to='/issue' className='btn_changed'>
+            Change issue
+          </Link>
+        </div>
+
+        <div className='containerSucces-results-single'>
+          <p>language: {language}</p>
+          <Link to='/language' className='btn_changed'>
+            Change language
+          </Link>
+        </div>
+      </div>
+
+      <div className='containerSucces-buttons'>
+        <Link to='/' className='btn_startSearchAgain'>
+          Start search again
+        </Link>
+      </div>
     </div>
   );
 };
